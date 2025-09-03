2841bbb876aaf117c6a52784f192ba0eaac2b861
ilyassatlassi <atlassiilyass@gmail.com>
2025-09-03 20:43:36 +0100

    add some change

--- src/types/tokens.rs
+++ src/types/tokens.rs
@@ -10,7 +10,17 @@ pub enum Token {
     Semicolon,
     EOF,
 }
-
+// enum Token {
+//     Command(String),    // e.g., "ls", "cd", "echo"
+//     Flag(String),       // e.g., "-l", "-a", "-la"  
+//     Argument(String),   // e.g., "/home/user", "file.txt"
+//     Pipe,               // "|"
+//     RedirectOut,        // ">"
+//     RedirectIn,         // "<"
+//     And,                // "&&"
+//     Or,                 // "||"
+//     Semicolon,          // ";"
+// }
 #[derive(Debug)]
 
 pub struct TokenWithPos {
